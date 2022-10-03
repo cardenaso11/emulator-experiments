de38b94b87d8667c0c0d56ba4ebfdb80822a3cb1
Sasha Lastochkina <60942960+sashalast@users.noreply.github.com>
2022-10-03 01:43:38 -0600

    added test

--- emulator-ts/test/decode.test.ts
+++ emulator-ts/test/decode.test.ts
@@ -1,6 +1,11 @@
 import { assert, expect, test } from 'vitest'
-import {decodeOpCode, OpCode} from '../src/Interpreter'
+import {decodeOpCode, OpCode, combine2Nibbles, combine3Nibbles} from '../src/Interpreter'
 
 test('decode opcode', () => {
-  expect(decodeOpCode(0x00E0)).toStrictEqual({type: "CLS"});
+  expect(decodeOpCode(0x00E0)).toStrictEqual({type: 'CLS'});
+  expect(decodeOpCode(0x00EE)).toStrictEqual({type: 'RET'});
+  expect(decodeOpCode(0x1987)).toStrictEqual({type: 'JP', addr: 0x987});
+  
+  expect(combine2Nibbles([0x0,0x2])).toStrictEqual(0x02);
+  expect(combine3Nibbles([0x9,0x8,0x7])).toStrictEqual(0x987);
 })
