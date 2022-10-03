d35aded11b324004eec5e1a7e74a6017c2b9e9ba
Sasha Lastochkina <60942960+sashalast@users.noreply.github.com>
2022-10-02 18:26:24 -0600

    make cls decode test pass

--- emulator-ts/test/decode.test.ts
+++ emulator-ts/test/decode.test.ts
@@ -2,5 +2,5 @@ import { assert, expect, test } from 'vitest'
 import {decodeOpCode, OpCode} from '../src/Interpreter'
 
 test('decode opcode', () => {
-  expect(decodeOpCode(0x00E0)).toStrictEqual({kind: "CLS"});
+  expect(decodeOpCode(0x00E0)).toStrictEqual({type: "CLS"});
 })
